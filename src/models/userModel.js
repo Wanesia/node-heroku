671f0eff7d1ada1e0bf8f444c22dcf37fe3e82a9
Wanesa Wintmiller <wintmiller161@gmail.com>
2024-10-04 01:49:12 +0200

    bcrypt hashing

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,10 +1,30 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
-const UserSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema({
   nickname: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
 });
 
-const User = mongoose.model("User", UserSchema);
+//pre-save middleware is a function that runs before a document is saved to the database
+userSchema.pre("save", async function (next) {
+  const user = this;
+
+  //prevents rehashing when updating other fields like email or nickname
+  if (!user.isModified("password")) {
+    return next();
+  }
+
+  // Generate salt and hash the password
+  try {
+    const salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(user.password, salt);
+    next();
+  } catch (err) {
+    next(err);
+  }
+});
+
+const User = mongoose.model("User", userSchema);
 module.exports = User;
