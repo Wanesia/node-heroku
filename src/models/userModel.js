e865dd6e779461a04f2085ba14a8eb6aef5452db
Wanesa Wintmiller <wintmiller161@gmail.com>
2024-10-05 00:56:02 +0200

    fixed hashing bug

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -7,25 +7,5 @@ const userSchema = new mongoose.Schema({
   password: { type: String, required: true },
 });
 
-//pre-save middleware is a function that runs before a document is saved to the database
-userSchema.pre("save", async function (next) {
-  const user = this;
-
-  //prevents rehashing when updating other fields like email or nickname
-  if (!user.isModified("password")) {
-    return next();
-  }
-
-  // Generate salt and hash the password
-  try {
-    console.log("in the try")
-    const salt = await bcrypt.genSalt(10);
-    user.password = await bcrypt.hash(user.password, salt);
-    next();
-  } catch (err) {
-    next(err);
-  }
-});
-
 const User = mongoose.model("User", userSchema);
 module.exports = User;
